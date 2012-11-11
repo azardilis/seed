49263bbed677af70315f2332cb5c7e9f3017c719
Alex Petroaica <alexandru.petroaica@gmail.com>
2012-11-11 20:13:34 +0000

    fixed import error in Post and Grade database models

--- forum/model/base/Grade.py
+++ forum/model/base/Grade.py
@@ -2,6 +2,6 @@ from google.appengine.ext import db
 import Assessment
 import User
 
-student = db.ReferenceProperty(User, collection_name='grades', required=True)
-assessmentSubject = db.ReferenceProperty(Assessment, collection_name='grades',required=True)
+student = db.ReferenceProperty(User.User, collection_name='grades', required=True)
+assessmentSubject = db.ReferenceProperty(Assessment.Assessment, collection_name='grades',required=True)
 mark = db.IntegerProperty(required=True)
--- forum/model/base/Post.py
+++ forum/model/base/Post.py
@@ -8,9 +8,9 @@ import User
 class Post(db.Model) :
 	subject = db.StringProperty(required=True)
 	body = db.StringProperty(required=True)
-	thread = db.ReferenceProperty(Thread, collection_name='posts', required=True)
+	thread = db.ReferenceProperty(Thread.Thread, collection_name='posts', required=True)
 	votes = db.IntegerProperty(required=True)
-	poster = db.ReferenceProperty(User, collection_name='posts', required=True)
+	poster = db.ReferenceProperty(User.User, collection_name='posts', required=True)
 	timestamp = db.DateTimeProperty(auto_now_add=True, required=True)
 	answer = db.BooleanProperty(required=True)
 	
