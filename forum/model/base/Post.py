fe827afebc74d00723f27ca372f5a36bbee69d5d
Dennis Perez Mavrogenis <dpm3g10@soton.ac.uk>
2012-11-26 14:35:57 +0000

    Changed classes thread and post

--- forum/model/base/Post.py
+++ forum/model/base/Post.py
@@ -3,16 +3,11 @@ from google.appengine.ext.db import polymodel
 import Thread
 import User
 
-
-
-
 class Post(polymodel.PolyModel) :
 	subject = db.StringProperty(required=True)
 	body = db.StringProperty(required=True)
-	thread = db.ReferenceProperty(Thread.Thread, collection_name='posts', required=True)
+	thread = db.ReferenceProperty(Thread.Thread, collection_name='posts')
 	votes = db.IntegerProperty(required=True)
 	poster = db.ReferenceProperty(User.User, collection_name='posts', required=True)
 	timestamp = db.DateTimeProperty(auto_now_add=True, required=True)
 	answer = db.BooleanProperty(required=True)
-	
-	
--- forum/model/base/Thread.py
+++ forum/model/base/Thread.py
@@ -2,6 +2,12 @@ import Post
 import Category
 from google.appengine.ext import db
 
-class Thread(Post.Post) :
+class Thread(db.Model) :
 	category = db.ReferenceProperty(Category.Category, collection_name='threads' ,required=True)
 	tags = db.ListProperty(str, required=True)
+	subject = db.StringProperty(required=True)
+        body = db.StringProperty(required=True)
+        thread = db.ReferenceProperty(Thread.Thread, collection_name='posts', required=True)
+        votes = db.IntegerProperty(required=True)
+        poster = db.ReferenceProperty(User.User, collection_name='posts', required=True)
+        timestamp = db.DateTimeProperty(auto_now_add=True, required=True) 
