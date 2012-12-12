da10baf75d1a1a2c41d540cf3e9aec58842e7f90
Dio Perez Mavrogenis <dpm3g10@ecs.soton.ac.uk>
2012-12-12 11:47:55 +0000

    made some progress with the controls

--- forum/functions/ForumFunctions.py
+++ forum/functions/ForumFunctions.py
@@ -4,7 +4,7 @@ from model.base.User import User
 from model.base.Post import Post
 
 def nl2br(txt) :
-	return re.sub(r'(\n|\r|\r\n|\n\r)','<br/>',txt)
+	return re.sub(r'(\r\n|\n\r|\n|\r)','<br/>',txt)
 
 def get_posts(tid):
     t = Thread.get_by_id(int(tid))
@@ -17,8 +17,9 @@ def get_children(plist , posts):
 	for p in plist :
 		posts = make_article(posts)
 		posts = make_post(p,posts)
+		posts = make_toolbar(p,posts)
 
-		if p.replies : 
+		if len(p.replies.fetch(1) ) > 0 : #little dirty hack to check if any responses exist
 			posts = make_section(posts)
 			for r in p.replies :
 				a = list ()
@@ -29,12 +30,31 @@ def get_children(plist , posts):
 
 	return posts
 
+def make_toolbar(p,posts):
+	pid = str(p.key().id())
+	html =''' 
+	<section id="class">
+		<span class="controls">
+			<p class="reply" pid="'''+pid+'''">Reply</p>
+			<p class="quote" pid="'''+pid+'''">Quote</p>
+			<p class="voteup" pid="'''+pid+'''">Vote Up</p>
+			<p class="votedown" pid="'''+pid+'''">Vote Down</p>
+		</span>
+			<form id="rf'''+pid+'''" class="replyform" method="post" action="postreply">
+				<input type="hidden" value="'''+pid+'''" name="r2pid">
+				<textarea name="replybox" rows="5" cols="50"></textarea>
+				<input type="submit" value="Reply">
+			</form>
+	</section>
+	'''
+	return posts+html
+
 def make_article(posts):
-	posts += '<article class="post">'
+	posts += '<section class="post">'
 	return posts 
 
 def close_article(posts):
-	posts += '</article>'	
+	posts += '</section>'	
 	return posts
 
 def make_section(posts):
@@ -45,7 +65,8 @@ def close_section(posts) :
 	posts += '</section>'
 	return posts
 
+#make show all the deatils here)
 def make_post(p,posts) :
-	posts += nl2br(p.body)
+	posts += '<article class="art" poster="'+p.poster.key().name()+'" pid='+str(p.key().id())+' id="'+str(p.key().id())+'">'+nl2br(p.body)+'</article>'
 	return posts
 	
