eabd926e6150fdff31531eec1c3f919aada7282f
Dio Perez Mavrogenis <dpm3g10@ecs.soton.ac.uk>
2012-12-12 08:55:20 +0000

    added multiline support for posts and threads

--- forum/model/base/Post.py
+++ forum/model/base/Post.py
@@ -4,7 +4,7 @@ import User
 import Post 
 
 class Post(db.Model) :
-	body = db.StringProperty(required=True)
+	body = db.StringProperty(required=True,multiline=True)
 	thread = db.ReferenceProperty(Thread.Thread, collection_name='posts')
 	votes = db.IntegerProperty(default=0)
 	reply = db.SelfReferenceProperty(collection_name = 'replies');
--- forum/model/base/Thread.py
+++ forum/model/base/Thread.py
@@ -5,7 +5,7 @@ from google.appengine.ext import db
 class Thread(db.Model) :
 	category = db.ReferenceProperty(Category.Category,required=True, collection_name='threads')
 	tags = db.ListProperty(str, required=True)
-	subject = db.StringProperty(required=True)
+	subject = db.StringProperty(required=True,multiline=True)
         body = db.StringProperty(required=True)
         votes = db.IntegerProperty(default=0)
         poster = db.ReferenceProperty(User.User, collection_name='threads', required=True)
