16dcee48086f5efe48d6ab960ccebaa89cc623b1
Dionisio Perez Mavrogenis <dpm3g10@ecs.soton.c.uk>
2012-12-25 16:30:35 +0200

    improved the modulebar

--- forum/js/modulebar_functions.js
+++ forum/js/modulebar_functions.js
@@ -8,9 +8,12 @@ $(document).ready(function(){
 		tw += $(this).width();
 	});
 	$('ul#modulebar').css('width',tw + ($('ul#modulebar').children().size()*12));
+	curr = 0 ; /*counter that will hold the current position, initialized here*/
+	total = $('ul#modulebar').children().size(); /*total modules subscribed to*/
 
 });
 
 function scroll(dir){
-	$('#modulenav').scrollTo(dir+'=150px',200,{queue:true, axis:'x'});
+	curr = (curr + parseInt(dir))%total ;
+	$('#modulenav').scrollTo('li:eq('+curr+')',200,{queue:true, axis:'x'});
 }
