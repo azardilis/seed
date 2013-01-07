f0652261626b41732b260f76446a333c60a0f8b8
marinos <marinos@thinkpad.(none)>
2013-01-07 20:54:54 +0200

    made EcsCode attr not required

--- forum/model/base/Module.py
+++ forum/model/base/Module.py
@@ -4,7 +4,7 @@ from SchoolYear import SchoolYear
 
 class Module(db.Model) :
 	title = db.StringProperty(required=True)
-	ecsCode = db.StringProperty(required=True)
+	ecsCode = db.StringProperty(required=False)
 	ecs_page = db.LinkProperty(required=True)
 	yearCourseSemester = db.ReferenceProperty(YearCourseSemester.YearCourseSemester , collection_name='modules', required=True)
 	student_count = db.IntegerProperty(default=0) #must be increamented every time a student subscribes (eg when creating Subscriptions in populate_db)
